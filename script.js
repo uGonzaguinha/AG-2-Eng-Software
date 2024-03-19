779095ca878204ed39af3b1d041a19150e75c8e4
Gustavo Gonzaga <gustavogsantos2013@gmail.com>
2024-03-19 13:43:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculate(operation) {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var result;
+
+    switch(operation) {
+
+    }
+
+    document.getElementById('result').innerHTML = 'Resultado: ' + result;
+}
